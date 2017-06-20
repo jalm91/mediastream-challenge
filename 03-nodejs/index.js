a251402888d3c9d9d53e930adfae2097c36d306f
home <home@debian>
2017-06-20 14:54:52 -0500

    segunda parte

--- 03-nodejs/index.js
+++ 03-nodejs/index.js
@@ -18,6 +18,7 @@ $ node utils/seed.js
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+var csv = require('csv-express')
 
 // Setup database
 mongoose.Promise = Promise;
@@ -28,5 +29,40 @@ const User = require('./models/User');
 const app = express();
 
 // TODO
+
+app.use(morgan('dev'));
+app.get('/', function (req, res) {
+  res.send('Usuarios');
+});
+
+app.get('/crear', function (req, res) {
+	//creo usuarios aleatoriamente solo para verificarlos 
+	for (var i = 0; i < 25; i++) {
+		var user = new User({email: 'pruebas' + i + '@gmail.com',password:'123pruebas'});
+		user.save(function(err,user){
+		});	
+	}
+  res.send('usuario creado exitosamente');
+});
+app.get('/consultar', function (req, res) {
+	User.find(function(err,usuarios){
+		console.log("usuarios lista");
+		console.log(usuarios);
+	})
+  res.send('Usuarios -- consulta');
+});
+
+app.get('/users', function(req, res) {
+	User.find(function(err,usuarios){
+		var usrTmp = [];
+		for (var i = 0; i < usuarios.length; i++) {
+			usrTmp.push({
+				"No.": i,
+				"email":usuarios[i].email
+			})
+		}
+		res.csv(usrTmp, true);
+	})
+})
 
 app.listen(3000);
